d628562b267cd04d8e5bac8dce3815e8a4ab6653
P J Borowiecki <piotr.borowiecki@yahoo.com>
2023-10-28 12:57:38 +0200

    Update database schemas and left sidebar

--- src/actions/question.ts
+++ src/actions/question.ts
@@ -1,5 +1,30 @@
 "use server"
 
-export async function createQuestionAction() {
-  return "hello"
+import { prisma } from "@/db/prisma"
+import { type Question } from "@prisma/client"
+
+export async function createQuestionAction({ title, explanation, userId }) {
+  return await prisma.question.create({})
+}
+
+export async function getAllQuestionsAction(): Promise<
+  Question[] | null | undefined
+> {
+  return await prisma.question.findMany()
+}
+
+export async function getQuestionById(
+  id: string
+): Promise<Question | null | undefined> {
+  return await prisma.question.findUnique({
+    where: { id },
+  })
+}
+
+export async function getAllQuestionsByUserId(
+  userId: string
+): Promise<Question[] | null | undefined> {
+  return await prisma.question.findMany({
+    where: { userId },
+  })
 }
--- src/components/nav/navigation-sidebar.tsx
+++ src/components/nav/navigation-sidebar.tsx
@@ -18,10 +18,6 @@ export function SidebarNavigation({ userId }: SidebarNavigationProps) {
   return (
     <nav className="flex flex-1 flex-col gap-3">
       {siteConfig.navItemsSidebar.map((item: NavItem) => {
-        const active =
-          (pathname.includes(item.href) && item.href.length > 1) ||
-          pathname === item.href
-
         const Icon = Icons[item.icon as keyof typeof Icons]
 
         return (
@@ -32,7 +28,7 @@ export function SidebarNavigation({ userId }: SidebarNavigationProps) {
             }
             className={cn(
               "flex items-center justify-start gap-4 rounded-lg bg-transparent p-4 hover:bg-customLight-700 dark:hover:bg-customDark-300",
-              active
+              pathname === item.href
                 ? "primary-gradient text-customLight-900"
                 : "text-customDark-400 dark:text-customLight-900"
             )}
@@ -40,14 +36,14 @@ export function SidebarNavigation({ userId }: SidebarNavigationProps) {
             <Icon
               className={cn(
                 "h-5 min-h-[20px] w-5 min-w-[20px]",
-                active ? "" : "invert dark:invert-0"
+                item.href ? "" : "invert dark:invert-0"
               )}
               aria-hidden="true"
             />
             <span
               className={cn(
                 "text-[18px] font-medium leading-[25.2px] max-lg:hidden",
-                active && "font-bold leading-[140%]"
+                item.href && "font-bold leading-[140%]"
               )}
             >
               {item.title}
